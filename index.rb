68983add85ac93a855bea1eacacac68bf06d22db
CamilleAbella <camille.abella@hotmail.fr>
2019-12-25 18:42:25 +0100

    add exception message and rename index

--- index.rb
+++ index.rb
@@ -8,13 +8,13 @@ bot = Discordrb::Bot.new token: data['token']
 
 bot.message(start_with: /^eval\s+\S+/) do |message|
   if data['authorized'].include? message.author.id
-    message.content.strip!
     message.content.sub! /^eval\s+/, ''
+    message.content.strip!
     response = ''
     begin
       response += eval(message.content)
-    rescue
-      response = 'Error...'
+    rescue StandardError => error
+      response = error.message
     end
     message.respond response
   else
