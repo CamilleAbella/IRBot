7ef5128d479649f034539b45576f0f5e8dda566d
CamilleAbella <camille.abella@hotmail.fr>
2019-12-25 18:31:40 +0100

    test first command

--- ping.rb
+++ ping.rb
@@ -6,8 +6,20 @@ file = File.open "./config.json"
 data = JSON.load file
 bot = Discordrb::Bot.new token: data['token']
 
-bot.message(content: 'ping') do |event|
-  event.respond 'Pong!'
+bot.message(start_with: /^eval\s+\S+/) do |message|
+  if data['authorized'].include? message.author.id
+    message.content.strip!
+    message.content.sub! /^eval\s+/, ''
+    response = ''
+    begin
+      response += eval(message.content)
+    rescue
+      response = 'Error...'
+    end
+    message.respond response
+  else
+    message.respond 'You must be included in the authorized list.'
+  end
 end
 
 bot.run
